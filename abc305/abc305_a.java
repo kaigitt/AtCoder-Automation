626ef6ba7cd83adda63fb1d9c9245983502922aa
AutoCloneBot <acautoclone@bot.com>
2023-09-14 14:46:04 +0000

    Added my codes from AtCoder

---
+++ abc305/abc305_a.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(Math.round(n / 5.0) * 5);
+	}
+}
